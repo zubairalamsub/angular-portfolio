61026ed8e07eb13b010c6dc12015979c04bc38e7
Zubair Alam <zubairalam025@gmil.com>
2025-12-27 02:16:48 +0600

    Add environment files for Vercel deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  geminiApiKey: '__GEMINI_API_KEY__'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  geminiApiKey: '__GEMINI_API_KEY__'
+};
